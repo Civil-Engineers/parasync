61236cd5b152bbdccc93629b93231959905c0730
enderpicture <enderpicture@gmail.com>
2022-01-22 01:55:39 -0800

    Added DotTween

--- Assets/Prefab/Mover/Movement.cs
+++ Assets/Prefab/Mover/Movement.cs
@@ -6,53 +6,34 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
 
+    [SerializeField] private float speed = 5;
 
     // Start is called before the first frame update
     void Start()
     {
     }
 
-    private void Update() {
-        
+    private void Update()
+    {
+
     }
-
-    // public void OnMove(InputAction.CallbackContext context)
-    // {
-    //     Vector2 direction = context.ReadValue<Vector2>();
-    //     if (direction.y > 1)
-    //     {
-    //         Up();
-    //     }
-    //     if (direction.y < 1)
-    //     {
-    //         Down();
-    //     }
-    //     if (direction.x > 1)
-    //     {
-    //         Right();
-    //     }
-    //     if (direction.x < 1)
-    //     {
-    //         Left();
-    //     }
-    // }
-
 
     public void OnUp()
     {
-        Debug.Log("hi");
-        transform.position += new Vector3(6.4f/2, 3.2f/2, 0);
+        transform.position += new Vector3(0, 0, speed);
     }
     public void OnDown()
     {
-        transform.position += new Vector3(-6.4f/2, -3.2f/2, 0);
+        transform.position += new Vector3(0, 0, -speed);
     }
     public void OnLeft()
     {
-        transform.position += new Vector3(-6.4f/2, 3.2f/2, 0);
+        transform.position += new Vector3(-speed, 0, 0);
+        transform.rotation = Quaternion.Euler(0, 0, 0);
     }
     public void OnRight()
     {
-        transform.position += new Vector3(6.4f/2, -3.2f/2, 0);
+        transform.position += new Vector3(speed, 0, 0);
+        transform.rotation = Quaternion.Euler(0, -180, 0);
     }
 }
