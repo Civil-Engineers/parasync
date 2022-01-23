8ef84cf1e128185408b2cdf909db84aca2edb963
andy <theninjawoohoo@hotmail.com>
2022-01-22 22:18:40 -0800

    Fixed Enemy movements

--- Assets/Scripts/EnemyScripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScripts/EnemyScript.cs
@@ -1,61 +1,92 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class EnemyScript : MonoBehaviour
 {
     public Timer timerObject;
-    public int speed = 1;
-    public PlayerMove playerObj;
+    public PlayerMove playerObject;
+    [SerializeField] private Transform sprite;
+    [SerializeField] private int speed = 1;
+    [SerializeField] private GameObject enemyObject;
     // Start is called before the first frame update
+    [SerializeField] private MovementAction[] actions;
+
+    private Dictionary<string, MovementAction> _enemyActions;
+
+    private bool _isFacingRight = true;
     void Start()
     {
-        
+        _enemyActions = new Dictionary<string, MovementAction>();
+
+        foreach (MovementAction action in actions)
+            _enemyActions.Add(action.actionName, action);
     }
 
     // Update is called once per frame
     void Update()
     {
         if(!timerObject.IsTimerRunning) {
-            Vector3 upDir = new Vector3(transform.position.x, 0, transform.position.z + 1); 
-            Vector3 downDir = new Vector3(transform.position.x, 0, transform.position.z - 1); 
-            Vector3 leftDir = new Vector3(transform.position.x - 1, 0, transform.position.z); 
-            Vector3 rightDir = new Vector3(transform.position.x + 1, 0, transform.position.z); 
+            float currX = enemyObject.transform.position.x;
+            float currZ = enemyObject.transform.position.z;
+            Vector3 upDir = new Vector3(currX, 0, currZ + 1); 
+            Vector3 downDir = new Vector3(currX, 0, currZ - 1); 
+            Vector3 leftDir = new Vector3(currX - 1, 0, currZ); 
+            Vector3 rightDir = new Vector3(currX + 1, 0, currZ); 
 
-            float magnitudeUp = (playerObj.transform.position - upDir).magnitude;
-            float magnitudeDown = (playerObj.transform.position - downDir).magnitude;
-            float magnitudeLeft = (playerObj.transform.position - leftDir).magnitude;
-            float magnitudeRight = (playerObj.transform.position - rightDir).magnitude;
+            float magnitudeUp = (playerObject.transform.position - upDir).magnitude;
+            float magnitudeDown = (playerObject.transform.position - downDir).magnitude;
+            float magnitudeLeft = (playerObject.transform.position - leftDir).magnitude;
+            float magnitudeRight = (playerObject.transform.position - rightDir).magnitude;
 
             float[] magArray = new float[] { magnitudeUp, magnitudeDown, magnitudeLeft, magnitudeRight };
             float minMag = Mathf.Min(magArray);
             
-            Debug.Log(minMag.ToString());
-            if(minMag == magnitudeUp) { MoveUp(); }
-            else if(minMag == magnitudeDown) { MoveDown(); }
-            else if(minMag == magnitudeRight) { MoveRight(); }
-            else { MoveLeft(); }
+            Debug.Log(minMag);
+            if(minMag == magnitudeUp) { 
+                Vector3 newPos = _enemyActions["Move Forward"].TriggerAction(enemyObject);
+                Tween(newPos, _enemyActions["Move Forward"].faceRight);
+            }
+            else if(minMag == magnitudeDown) {  
+                Vector3 newPos = _enemyActions["Move Backward"].TriggerAction(enemyObject);
+                Tween(newPos, _enemyActions["Move Backward"].faceRight);
+            }
+            else if(minMag == magnitudeRight) {  
+                Vector3 newPos = _enemyActions["Move Right"].TriggerAction(enemyObject);
+                Tween(newPos, _enemyActions["Move Right"].faceRight);
+            }
+            else if(minMag == magnitudeLeft) {
+                Vector3 newPos = _enemyActions["Move Left"].TriggerAction(enemyObject);
+                Tween(newPos, _enemyActions["Move Left"].faceRight);
+            } else {
+                Debug.Log("We have a problem dude.");
+            }
             timerObject.IsTimerRunning = true;
             timerObject.ResetTime();
         }   
     }
 
-    public void MoveUp()
+    private void Tween(Vector3 target, bool faceRight)
     {
-        transform.position += new Vector3(0, 0, speed);
-    }
-    public void MoveDown()
-    {
-        transform.position += new Vector3(0, 0, -speed);
-    }
-    public void MoveLeft()
-    {
-        transform.position += new Vector3(-speed, 0, 0);
-        transform.rotation = Quaternion.Euler(0, 0, 0);
-    }
-    public void MoveRight()
-    {
-        transform.position += new Vector3(speed, 0, 0);
-        transform.rotation = Quaternion.Euler(0, -180, 0);
+        float time = 0.2f;
+
+        if (!_isFacingRight && faceRight)
+        {
+            _isFacingRight = true;
+            sprite.DORotate(new Vector3(0, -90, 0), time).SetEase(Ease.InQuad);
+            sprite.DOScale(new Vector3(1, 1, 1), 0).SetDelay(time);
+            sprite.DORotate(new Vector3(0, 90, 0), 0).SetDelay(time).SetEase(Ease.OutQuad);
+            sprite.DORotate(new Vector3(0, 0, 0), time).SetDelay(time);
+        }
+        else if (_isFacingRight && !faceRight)
+        {
+            _isFacingRight = false;
+            sprite.DORotate(new Vector3(0, -90, 0), time).SetEase(Ease.InQuad);
+            sprite.DOScale(new Vector3(-1, 1, 1), 0).SetDelay(time);
+            sprite.DORotate(new Vector3(0, 90, 0), 0).SetDelay(time).SetEase(Ease.OutQuad);
+            sprite.DORotate(new Vector3(0, 0, 0), time).SetDelay(time);
+        }
+        enemyObject.transform.DOMove(target, 0.15f);
     }
 }
