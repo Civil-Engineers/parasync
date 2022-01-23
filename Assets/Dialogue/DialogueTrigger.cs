dbbbbde9935fbd4c88076ba41df7229e54f883e4
jeremyxho <jeremyxho@gmail.com>
2022-01-23 13:21:26 -0800

    Dialogue stuff

---
+++ Assets/Dialogue/DialogueTrigger.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    [Header("Ink JSON")]
+    //[SerializeField] public TextAsset inkJSON;
+    [SerializeField] public TextAsset inkJSON;
+    private bool playerInRange;
+
+    private void Awake()
+    {
+        playerInRange = false;
+    }
+
+    private void Update()
+    {
+        if (playerInRange && !DialogueManager.GetInstance().dialogueIsPlaying)
+        {
+            if (Input.GetKeyDown("e"))
+            {
+                DialogueManager.GetInstance().EnterDialogueMode(inkJSON);
+            }
+        }    
+    }
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            playerInRange = true;
+        }
+    }
+    
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            playerInRange = false;
+        }
+    }
+}
---
+++ Assets/Dialogue/DialogueVariables.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ink.Runtime;
+
+public class DialogueVariables
+{
+    public Ink.Runtime.Object introSeen;
+    public Ink.Runtime.Object juniorSeen;
+
+    public void SaveVariablesFromStory(Story story)
+    {
+
+        this.introSeen = story.variablesState.GetVariableWithName("introSeen");
+        this.juniorSeen = story.variablesState.GetVariableWithName("juniorSeen");
+    }
+
+    public void LoadVariablesToStory(ref Story story)
+    {
+        if (introSeen != null)
+        {
+            story.variablesState.SetGlobal("introSeen", introSeen);
+            story.variablesState.SetGlobal("juniorSeen", juniorSeen);
+        }
+    }
+}
