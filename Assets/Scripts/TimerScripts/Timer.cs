c1f3c4b54481f237faed9f0392d2189efe5474cf
Ricky Chon <rickychon99@gmail.com>
2022-01-23 03:34:33 -0500

    Refactor Timer script for ActionManager preparation

--- Assets/Scripts/TimerScripts/Timer.cs
+++ Assets/Scripts/TimerScripts/Timer.cs
@@ -3,42 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+
 public class Timer : MonoBehaviour
 {   
-    private const float maxSeconds = 5.00f;
-    [SerializeField] private float secondsRemaining = 5.00f;
-    public bool IsTimerRunning = true;
+    [SerializeField] private float turnTimeInSeconds = 5.00f;
+    [SerializeField] private Image timerImage;
+    [SerializeField] private InputReader inputReader;
 
-    public Image timer;
-    public InputReader inputReader;
+    [HideInInspector] public bool isTimerRunning = true;
+
+    private float _maxTime;
+    private float _timeRemaining;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       _maxTime = turnTimeInSeconds;
+       _timeRemaining = turnTimeInSeconds;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(IsTimerRunning) {
-            
-            if(secondsRemaining >= 0) {
-                secondsRemaining -= Time.deltaTime;
-                timer.fillAmount = secondsRemaining / maxSeconds;
+        if (isTimerRunning) {
+            if (_timeRemaining >= 0) {
+                _timeRemaining -= Time.deltaTime;
+                timerImage.fillAmount = _timeRemaining / _maxTime;
             }
             else {
-                secondsRemaining = 0.00f;
-                IsTimerRunning = false;
+                _timeRemaining = 0.00f;
+                isTimerRunning = false;
                 inputReader.ResetPlayerMove();
             }
         } 
     }
 
-    void DisplaySeconds(float secondsToDisplay) {
-        float seconds = Mathf.FloorToInt(secondsToDisplay % 60);
-    }
-
     public void ResetTime() {
-        secondsRemaining = 5.00f;
+        _timeRemaining = turnTimeInSeconds;
     }
 }
