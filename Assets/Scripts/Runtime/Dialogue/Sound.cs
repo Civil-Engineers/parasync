f001a45ba759eabff25a6dcabad30d45f0349a8f
Ricky Chon <rickychon99@gmail.com>
2022-01-29 01:19:00 -0500

    Reimport packages and link their assemblies, namespace dialogue scripts

---
+++ Assets/Scripts/Runtime/Dialogue/Sound.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+
+namespace Parasync.Runtime.Dialogue
+{
+    [System.Serializable]
+    public class Sound
+    {
+        public string name;
+        public AudioClip clip;
+        [Range(0f, 1f)]
+        public float volume;
+        [Range(.1f, 3f)]
+        public float pitch;
+
+        [HideInInspector]
+        public AudioSource source;
+    }
+}
